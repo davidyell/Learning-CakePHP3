bd7cd0828971296bd98d695a4cdb6361a49cffd2
David Yell <dyell@ukwebmedia.com>
2014-04-04 12:51:09 +0100

    Added a database dump, until I figure out Console/cake

---
+++ db_dump/cake3-dev2_2014-04-04.sql
@@ -0,0 +1,155 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.6.17)
+# Database: cake3-dev2
+# Generation Time: 2014-04-04 11:50:15 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table answers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `answers`;
+
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer` text NOT NULL,
+  `accepted` tinyint(1) NOT NULL DEFAULT '0',
+  `question_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `upvotes` int(11) NOT NULL DEFAULT '0',
+  `downvotes` int(11) NOT NULL DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users_idx` (`user_id`),
+  KEY `fk_questions_idx` (`question_id`),
+  CONSTRAINT `fk_answers_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_answers_questions` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+
+INSERT INTO `answers` (`id`, `answer`, `accepted`, `question_id`, `user_id`, `upvotes`, `downvotes`, `created`, `modified`)
+VALUES
+	(1,'You make a question just how you asked this question!',0,1,3,0,0,'2014-04-04 11:42:57','2014-04-04 11:42:57');
+
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table comments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) DEFAULT NULL,
+  `answer_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` varchar(255) NOT NULL DEFAULT '',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_question_idx` (`question_id`),
+  KEY `fk_answer_idx` (`answer_id`),
+  KEY `fk_comments_user_idx` (`user_id`),
+  CONSTRAINT `fk_comments_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_answer` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+
+INSERT INTO `comments` (`id`, `question_id`, `answer_id`, `user_id`, `comment`, `created`, `modified`)
+VALUES
+	(1,1,NULL,1,'I think you\'ve answered your own question there','2014-04-04 11:42:20','2014-04-04 11:42:20'),
+	(2,1,NULL,3,'Haha','2014-04-04 12:19:51','2014-04-04 12:19:51');
+
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table questions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `questions`;
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `question` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `upvotes` int(11) NOT NULL DEFAULT '0',
+  `downvotes` int(11) NOT NULL DEFAULT '0',
+  `views` int(11) NOT NULL DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users_idx` (`user_id`),
+  CONSTRAINT `fk_questions_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+
+INSERT INTO `questions` (`id`, `title`, `question`, `user_id`, `upvotes`, `downvotes`, `views`, `created`, `modified`)
+VALUES
+	(1,'How do I make a question?','I\'ve been trying to make a question and I just can\'t figure it out. Can anyone help me out with how to create a question?',2,0,0,0,'2014-04-04 11:42:06','2014-04-04 11:42:06');
+
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created`, `modified`)
+VALUES
+	(1,'Neon','neon1024@gmail.com','neon1024','2014-04-04 11:41:03','2014-04-04 11:41:03'),
+	(2,'Geoff','geoff@example.com','geoff','2014-04-04 11:41:15','2014-04-04 11:41:15'),
+	(3,'Bruce','bruce@example.com','bruce','2014-04-04 11:41:27','2014-04-04 11:41:27');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
