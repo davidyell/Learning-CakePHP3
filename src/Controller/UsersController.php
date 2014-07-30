f2b7e17919c67703d70ef939a66b325fa763abb2
David Yell <dyell@ukwebmedia.com>
2014-07-30 11:22:28 +0100

    Updated the login method with the new auth identify()

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -34,10 +34,12 @@ class UsersController extends AppController {
  */
 	public function login() {
 		if ($this->request->is('post')) {
-			if ($this->Auth->login()) {
-				return $this->redirect(['controller' => 'questions', 'action' => 'index']);
+			$user = $this->Auth->identify();
+			if ($user) {
+				$this->Auth->setUser($user);
+				return $this->redirect($this->Auth->redirectUrl());
 			} else {
-				$this->Session->setFlash(__('Could not login'), 'flash', ['class' => 'error']);
+				$this->Flash->error(__('Username or password is incorrect'), 'default', [], 'auth');
 			}
 		}
 	}
