54a192854505fae6d8609d1ff3bd9aeef8356678
David Yell <neon1024@gmail.com>
2014-08-24 15:41:36 +0200

    Formatting the code

--- webroot/js/common.js
+++ webroot/js/common.js
@@ -1,32 +1,32 @@
-$(function () {
-	
+$(function() {
+
 // Ajax globals
-	$(document).ajaxStart(function () {
-		$('#loading').show();
-	});
-	$(document).ajaxStop(function () {
-		$('#loading').hide();
-	});
-	$(document).ajaxError(function () {
-		$('#login').show();
-	});
-	
+    $(document).ajaxStart(function() {
+        $('#loading').show();
+    });
+    $(document).ajaxStop(function() {
+        $('#loading').hide();
+    });
+    $(document).ajaxError(function() {
+        $('#login').show();
+    });
+
 // Ajax voting
-	$('.votes a').click(function (e) {
-		e.preventDefault();
-		
-		$(this).addClass('voted');
-		var linkElement = $(this);
-		var url = '/' + $(this).data('controller') + '/vote/' + $(this).data('dir') + '/' + $(this).data('id') + '.json';
-		
-		$.get(url, function (data, status) {
-			$(linkElement).siblings('span.votes').html(data.votes);
-		}, 'json');
-	});
-	
+    $('.votes a').click(function(e) {
+        e.preventDefault();
+
+        $(this).addClass('voted');
+        var linkElement = $(this);
+        var url = '/' + $(this).data('controller') + '/vote/' + $(this).data('dir') + '/' + $(this).data('id') + '.json';
+
+        $.get(url, function(data, status) {
+            $(linkElement).siblings('span.votes').html(data.votes);
+        }, 'json');
+    });
+
 // Dialogs
-	$('.dialog span.glyphicon.glyphicon-remove').click(function () {
-		$(this).parent().fadeOut();
-	});
-	
+    $('.dialog span.glyphicon.glyphicon-remove').click(function() {
+        $(this).parent().fadeOut();
+    });
+
 });
