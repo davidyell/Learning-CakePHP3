7434d53c20b51dd558c884415dfd173b5c00fdec
David Yell <dyell@ukwebmedia.com>
2014-10-09 16:58:59 +0100

    Only need to include the one file as the others are mixins

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')
 
 gulp.task('sass', function () {
-    gulp.src('./webroot/sass/*.scss')
+    gulp.src('./webroot/sass/style.scss')
         .pipe(sass({
         	outputStyle: 'compressed'
         }))
